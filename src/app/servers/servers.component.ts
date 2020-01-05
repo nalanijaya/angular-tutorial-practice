74951c5acf11f9113e376d617d21044a3c9ef012
NJ <nalani.jayasundara@gmail.com>
2020-01-05 19:49:36 +0530

    angular-tutorial-practice

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ServersService } from './servers.service';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  private servers: {id: number, name: string, status: string}[] = [];
+
+  constructor(private serversService: ServersService) { }
+
+  ngOnInit() {
+    this.servers = this.serversService.getServers();
+
+  }
+
+}
